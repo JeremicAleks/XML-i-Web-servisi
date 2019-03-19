0ca67ea0b620ceb7b7a3e17575854b5b354f0a69
kiriyaga <38253493+kiriyaga@users.noreply.github.com>
2019-03-19 13:57:21 +0100

    Projekti za PKI

---
+++ PKI/pki-api/pki-api/src/main/java/pki/PkiApiApplication.java
@@ -0,0 +1,13 @@
+package pki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiApiApplication.class, args);
+	}
+
+}
