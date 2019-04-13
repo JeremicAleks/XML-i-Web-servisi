941daf74d177e041a4ec4b1fd8e8284081001b60
kiriyaga <38253493+kiriyaga@users.noreply.github.com>
2019-04-13 22:52:53 +0200

    https server-client, server-server

---
+++ agent-api/agent-api/src/main/java/com/agentapi/config/WebAuthConfiguration.java
@@ -0,0 +1,29 @@
+package com.agentapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebAuthConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/test/**").permitAll()
+				.antMatchers(HttpMethod.OPTIONS, "/h2-console/**").permitAll().and()
+				.cors();
+		httpSecurity.headers().cacheControl();
+		httpSecurity.headers().frameOptions().disable();
+		// Custom JWT based authentication
+		//httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+	}
+
+
+}
---
+++ agent-api/central-api/src/main/java/com/centralapi/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.centralapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/protect")
+public class TestController {
+	
+	@PreAuthorize("hasRole('ROLE_SSL_USER')")
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> findCertificates() {
+		
+        return new ResponseEntity<>("Its cool!", HttpStatus.OK);
+    }
+
+}
