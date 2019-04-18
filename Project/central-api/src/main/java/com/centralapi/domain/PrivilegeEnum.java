343a487934de99d8bd4d1e22bb720ceefe7cf226
marjanskid <marjanskid@uns.ac.rs>
2019-04-18 03:34:36 +0200

    Privileges

--- Project/central-api/src/main/java/com/centralapi/domain/PrivilegeEnum.java
+++ Project/central-api/src/main/java/com/centralapi/domain/PrivilegeEnum.java
@@ -1,7 +1,6 @@
 package com.centralapi.domain;
 
 public enum PrivilegeEnum {
-	
-	WRITE_PRODUCT,READ_PRODUCT,DELETE_PRODUCT
 
+	WRITE_PRODUCT, READ_PRODUCT, DELETE_PRODUCT
 }
--- Project/client-app/src/app/models/role.ts
+++ Project/client-app/src/app/models/role.ts
@@ -1,3 +1,11 @@
+import { PrivilegeEnum } from './privilege-enum';
+
 export class Role {
-    
+    id: number
+    name: String
+    privileges: Array<PrivilegeEnum>
+
+    constructor() {
+        this.privileges = Array<PrivilegeEnum>()
+    }
 }
