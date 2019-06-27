e925f2e5072d78986049e87d432f293946c46d57
kiriyaga <38253493+kiriyaga@users.noreply.github.com>
2019-06-27 05:02:03 +0200

    resetovanje lozinke

---
+++ Project/authorization-api/src/main/java/com/authorizationapi/domain/dto/ForgottenPasswordDTO.java
@@ -0,0 +1,17 @@
+package com.authorizationapi.domain.dto;
+
+public class ForgottenPasswordDTO {
+	
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+
+}
--- Project/authorization-api/src/main/java/com/authorizationapi/service/UserService.java
+++ Project/authorization-api/src/main/java/com/authorizationapi/service/UserService.java
@@ -2,6 +2,7 @@ package com.authorizationapi.service;
 
 import org.springframework.stereotype.Service;
 
+import com.authorizationapi.domain.dto.ChangePasswordDTO;
 import com.authorizationapi.domain.dto.RegisterUserDTO;
 import com.authorizationapi.domain.dto.UserLoginDTO;
 
@@ -10,5 +11,6 @@ public interface UserService {
 	
 	UserLoginDTO userLogin(String username, String password);
 	Object userRegister(RegisterUserDTO user);
+	Boolean changePassword(ChangePasswordDTO change);
 
 }
