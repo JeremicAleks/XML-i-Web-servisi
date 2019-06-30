33f7c7a918652582d3232bf657e272ba6641ffa9
Aleksandar Jeremic <JeremicAleks@users.noreply.github.com>
2019-06-30 09:39:33 +0200

    aaaaa loga Boze

---
+++ Project/client-app/src/app/models/filter-field.ts
@@ -0,0 +1,7 @@
+export class FilterField {
+
+    id:number;
+    description:string;
+    active:boolean = false;
+    
+}
--- Project/client-app/src/app/views/destinations/destinations.component.ts
+++ Project/client-app/src/app/views/destinations/destinations.component.ts
@@ -4,7 +4,8 @@ import { SearchService } from '../../services/search.service';
 import { SearchParams } from '../../models/search-params';
 import { FormControl, FormGroup, Validators, FormBuilder, FormArray, AbstractControl } from '@angular/forms';
 import { RoomService } from 'src/app/services/room.service';
-import {DomSanitizer} from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
+import { FilterField } from 'src/app/models/filter-field';
 
 @Component({
   selector: 'app-destinations',
@@ -24,48 +25,187 @@ export class DestinationsComponent implements OnInit {
     checkOut: new FormControl(new Date(), [Validators.required])
   });
 
-  typeForm: FormGroup;
-  categoryForm: FormGroup;
-  additionalServiceForm: FormGroup;
-  rooms:Array<any>;
 
-  types: any;
+  rooms: Array<any>;
+  roomsCopy: Array<any>;
+  categories: Array<FilterField> = new Array<FilterField>();
+  additionalServices: Array<FilterField> = new Array<FilterField>();
+  types: Array<FilterField> = new Array<FilterField>();
 
-  constructor(private DomSanitizer: DomSanitizer,private route: ActivatedRoute, private searchService: SearchService, private roomService: RoomService, private router: Router, private _formBuilder: FormBuilder) {
-
-    
-
-    this.typeForm = this._formBuilder.group({
-      typeArray: this._formBuilder.array([])
-    });
-
-    this.categoryForm = this._formBuilder.group({
-      categoryArray: this._formBuilder.array([])
-    });
-
-    this.additionalServiceForm = this._formBuilder.group({
-      additionalServiceArray: this._formBuilder.array([])
-    });
+  constructor(private DomSanitizer: DomSanitizer, private route: ActivatedRoute,
+    private searchService: SearchService, private roomService: RoomService,
+    private router: Router, private _formBuilder: FormBuilder) {
 
     this.roomService.getTypes().subscribe(
       data => {
-        this.types = data;
         data.forEach(element => {
-          console.log("Desc " + element.description)
-          this.addItemTypeForm();
+          this.types.push(element);
         });
       }
-    )
+    );
+
+    this.roomService.getCategories().subscribe(
+      data => {
+        data.forEach(element => {
+          this.categories.push(element);
+        });
+      }
+    );
+
+    this.roomService.getAdditionalServices().subscribe(
+      data => {
+        data.forEach(element => {
+          this.additionalServices.push(element);
+        });
+      }
+    );
   }
 
-  get typeArray() {
-    console.log("o bogo - get type array")
-    return this.typeForm.get('typeArray') as FormArray;
+  searchAgain() {
+    console.log('searchAgain clicked');
+    const checkInDate = this.searchForm.get('checkIn').value;
+    const checkOutDate = this.searchForm.get('checkOut').value;
+    const destination = this.searchForm.get('destination').value;
+
+    this.searchParams = new SearchParams(destination, checkInDate, checkOutDate, this.numOfPeople);
+
+    this.searchService.getSearchResults(this.searchParams).subscribe(
+      data => {
+        //alert('nesto se desilo kao');
+        this.rooms = data;
+        this.roomsCopy = data;
+        this.DomSanitizer.bypassSecurityTrustUrl(this.rooms[0].image[0]);
+        alert(this.rooms[0].location.name)
+      },
+      error => {
+        //alert('greska u pretrazivanju soba');
+      }
+    );
   }
 
-  addItemTypeForm() {
-    this.typeArray.push(this._formBuilder.control(new Boolean));
-    console.log('item type form - boolean pushed to type array')
+  resetFilter() {
+    console.log('resetovanje parametara filtriranja...');
+
+    for (let i = 0; i < this.types.length; i++) {
+      this.types[i].active = true;
+    }
+    for (let j = 0; j < this.categories.length; j++) {
+      this.categories[j].active = true;
+    }
+    for (let k = 0; k < this.additionalServices.length; k++) {
+      this.additionalServices[k].active = true;
+    }
+
+    this.roomsCopy = this.rooms;
+  }
+
+  filterClicked() {
+    console.log('clicked on room filter...');
+    this.FilterAll();
+    console.log('sobe su isfiltrirane...');
+    for (let r of this.roomsCopy) {
+      console.log(r.location.name);
+    }
+  }
+
+  ChangeMeType(i) {
+    if (this.types[i].active) {
+      this.types[i].active = false;
+    } else {
+      this.types[i].active = true;
+    }
+    alert(this.types[i].active);
+  }
+
+  ChangeMeCategories(i) {
+    if (this.categories[i].active) {
+      this.categories[i].active = false;
+    } else {
+      this.categories[i].active = true;
+    }
+    alert(this.categories[i].active);
+  }
+
+  ChangeMeAdditionalServices(i) {
+    if (this.additionalServices[i].active) {
+      this.additionalServices[i].active = false;
+    } else {
+      this.additionalServices[i].active = true;
+    }
+    alert(this.additionalServices[i].active);
+  }
+
+  FilterAll() {
+    //pronadji sve sto je aktivno sto se tice tipa
+    const tipovi = Array<FilterField>();
+    const categories = Array<FilterField>();
+    const additionalServices = Array<FilterField>();
+
+    this.types.forEach(element => {
+      if (element.active == false) {
+        tipovi.push(element);
+      }
+    });
+
+    alert(tipovi.length);
+
+    this.categories.forEach(element => {
+      if (element.active == false) {
+        categories.push(element);
+      }
+    });
+    alert(categories.length);
+
+    this.additionalServices.forEach(element => {
+      if (element.active == false) {
+        additionalServices.push(element);
+      }
+    })
+    alert(additionalServices.length)
+
+    //filtriraj po
+    this.FilterByType(tipovi, categories, additionalServices);
+
+  }
+
+  public FilterByType(types, categories, additionalServices) {
+
+    this.roomsCopy = new Array<any>();
+
+    this.rooms.forEach(element => {
+      let booleanRoom = false;
+
+      types.forEach(tipovi => {
+        if (element.accommodationType.description == tipovi.description) {
+          booleanRoom = true;
+          alert('pronasao u tipovima');
+        }
+      });
+
+      categories.forEach(kategorije => {
+        if (element.accommodationCategory.description == kategorije.description) {
+          booleanRoom = true;
+          alert('pronasao u kategorijama');
+        }
+      });
+
+      if (additionalServices == null) {
+        booleanRoom = true;
+        alert('nema servisa');
+      } else {
+        additionalServices.forEach(servisi => {
+          if (element.additionalService.description == servisi.description) {
+            booleanRoom = true;
+            alert('pronasao u servisima');
+          }
+        });
+      }
+
+      if (booleanRoom) {
+        this.roomsCopy.push(element);
+        alert('dodata bar jedna soba');
+      }
+    });
   }
 
   ngOnInit() {
@@ -99,19 +239,20 @@ export class DestinationsComponent implements OnInit {
       data => {
         //alert('nesto se desilo kao');
         this.rooms = data;
+        this.roomsCopy = data;
         this.DomSanitizer.bypassSecurityTrustUrl(this.rooms[0].image[0]);
         alert(this.rooms[0].location.name)
       },
       error => {
         //alert('jebem ti se s mamom');
-      
+
 
       }
     );
   }
 
   reservation(roomId) {
-    this.router.navigate(['/reservation/'+this.searchForm.get("checkIn").value+"/"+this.searchForm.get("checkOut").value+"/"+roomId]);
+    this.router.navigate(['/reservation/' + this.searchForm.get("checkIn").value + "/" + this.searchForm.get("checkOut").value + "/" + roomId]);
   }
 
 }
